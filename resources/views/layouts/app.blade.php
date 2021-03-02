8face0f3763c6e5ccd6fa8a476a1bc1b9a93cec1
Marsasefta <marsasefta01@gmail.com>
2021-03-02 20:39:44 +0700

    add link navbar ke data survey

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,14 +26,16 @@
             <nav class="navbar navbar-expand-lg bg-dark">
                 <div class="container">
                     <img class="logo" src="{{ asset('img/logo.png') }}">
+                    
                     <ul class="navbar-nav ml-auto">
+                        <a class="text-white" href="{{('/data-survey')}}">Data Survey</a>
                         @guest
                         @if (Route::has('login'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                         @endif
-
+                        
                         @if (Route::has('register'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
